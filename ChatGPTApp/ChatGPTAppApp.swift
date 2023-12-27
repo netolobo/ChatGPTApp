cd7ce2174f696608ce7e26bbf13c43897b09e140
netolobo <desilio@gmail.com>
2023-12-26 20:20:43 -0800

    Initial Commit

---
+++ ChatGPTApp/ChatGPTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAppApp.swift
+//  ChatGPTApp
+//
+//  Created by Neto Lobo on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatGPTApp
+//
+//  Created by Neto Lobo on 26/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
